2b87e34ec393121e55cede5e74cfdfd39e926a61
ramyahd <55663832+ramyahd@users.noreply.github.com>
2019-11-21 14:09:56 +0530

    Update deploy_test.groovy

--- vars/deploy_test.groovy
+++ vars/deploy_test.groovy
@@ -1,6 +1,6 @@
 def call(String msg = 'deploy_test') {
 echo "${msg}"
-               withCredentials([string(credentialsId: 'tomcat_test', variable: 'password')]){
+               withCredentials([string(credentialsId: 'tomcat_dev', variable: 'password')]){
     sh 'sshpass -p ${password} ssh -o StrictHostKeyChecking=no ansadmin@172.31.3.247 \"ansible-playbook opt/playbooks/test.yml\"'
 
 }

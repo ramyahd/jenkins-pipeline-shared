d68ce4ec8cc49afdbeefa03d933c6db21db1c480
ramyahd <55663832+ramyahd@users.noreply.github.com>
2019-11-21 11:44:25 +0530

    Update deploy_development.groovy

--- vars/deploy_development.groovy
+++ vars/deploy_development.groovy
@@ -1,7 +1,7 @@
 def call(String msg = 'deploy_dev') {
 echo "${msg}"
   withCredentials([string(credentialsId: 'tomcat_dev', variable: 'password')]){
-    sh 'sshpass -p ${password} ssh -o StrictHostKeyChecking=no ansadmin@172.31.37.95 -C \"ansible-playbook /opt/playbooks/download.yml\"'
+    sh 'sshpass -p ${password} ssh -o StrictHostKeyChecking=no ansadmin@172.31.3.247-C \"ansible-playbook /opt/playbooks/download.yml\"'
   
   }
             }
